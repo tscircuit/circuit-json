baec7811ae71c57dd56d5371d895fa36edbb633e
seveibar <seveibar@gmail.com>
2024-05-05 17:12:40 -0700

    fix route hint point to make to layer optional

--- src/pcb/pcb_trace_hint.ts
+++ src/pcb/pcb_trace_hint.ts
@@ -2,20 +2,14 @@ import { z } from "zod"
 import { point } from "../common"
 import { distance } from "../units"
 import { layer_ref } from "./properties/layer_ref"
+import { route_hint_point } from "./index"
 
 export const pcb_trace_hint = z
   .object({
     type: z.literal("pcb_trace_hint"),
     pcb_port_id: z.string(),
     pcb_component_id: z.string(),
-    route: z.array(
-      z.object({
-        x: distance,
-        y: distance,
-        via: z.boolean().optional(),
-        to_layer: layer_ref.optional(),
-      })
-    ),
+    route: z.array(route_hint_point),
   })
   .describe("A hint that was used to generate a PCB trace")
 
