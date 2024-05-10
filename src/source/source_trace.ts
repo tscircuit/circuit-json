5068d9da0ea97d18b96a96be809a87b6a611440b
seveibar <seveibar@gmail.com>
2024-05-09 17:37:00 -0700

    add source net ids to source_trace

--- src/source/source_trace.ts
+++ src/source/source_trace.ts
@@ -4,6 +4,7 @@ export const source_trace = z.object({
   type: z.literal("source_trace"),
   source_trace_id: z.string(),
   connected_source_port_ids: z.array(z.string()),
+  connected_source_net_ids: z.array(z.string()),
 })
 
 export type SourceTrace = z.infer<typeof source_trace>
