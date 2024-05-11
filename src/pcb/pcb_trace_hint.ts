a4c11b0c75e9cbede629aee1ba4d2ef16e998532
seveibar <seveibar@gmail.com>
2024-05-11 12:37:11 -0700

    add pcb_trace_hint_id

--- src/pcb/pcb_trace_hint.ts
+++ src/pcb/pcb_trace_hint.ts
@@ -6,12 +6,13 @@ import { route_hint_point } from "./index"
 
 export const pcb_trace_hint = z
   .object({
+    pcb_trace_hint_id: z.string(),
     type: z.literal("pcb_trace_hint"),
     pcb_port_id: z.string(),
     pcb_component_id: z.string(),
     route: z.array(route_hint_point),
   })
-  .describe("A hint that was used to generate a PCB trace")
+  .describe("A hint that can be used to generate a PCB trace")
 
 export type PcbTraceHint = z.infer<typeof pcb_trace_hint>
 export type PcbTraceHintInput = z.input<typeof pcb_trace_hint>
