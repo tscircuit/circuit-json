f5d991ac6c5d92ed6c39e0f163d94a086512a07d
seveibar <seveibar@gmail.com>
2024-05-15 14:42:35 -0700

    font size can be specified in mm

--- src/pcb/pcb_silkscreen_text.ts
+++ src/pcb/pcb_silkscreen_text.ts
@@ -1,12 +1,13 @@
 import { z } from "zod"
 import { visible_layer } from "./properties/layer_ref"
 import { point } from "src/common"
+import { distance } from "src/units"
 
 export const pcb_silkscreen_text = z
   .object({
     type: z.literal("pcb_silkscreen_text"),
     font: z.literal("tscircuit2024").default("tscircuit2024"),
-    font_size: z.number().default(1),
+    font_size: distance.default("1mm"),
     pcb_component_id: z.string(),
     text: z.string(),
     layer: visible_layer,
