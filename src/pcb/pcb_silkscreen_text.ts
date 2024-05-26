2f708542f9d6b4de99ee00c1372986ca3d34d5bf
seveibar <seveibar@gmail.com>
2024-05-26 11:15:51 -0700

    fix: add anchor position and alignment

--- src/pcb/pcb_silkscreen_text.ts
+++ src/pcb/pcb_silkscreen_text.ts
@@ -7,11 +7,14 @@ export const pcb_silkscreen_text = z
   .object({
     type: z.literal("pcb_silkscreen_text"),
     font: z.literal("tscircuit2024").default("tscircuit2024"),
-    font_size: distance.default("1mm"),
+    font_size: distance.default("0.2mm"),
     pcb_component_id: z.string(),
     text: z.string(),
     layer: visible_layer,
-    center: point,
+    anchor_position: point.default({ x: 0, y: 0 }),
+    anchor_alignment: z
+      .enum(["center", "top_left", "top_right", "bottom_left", "bottom_right"])
+      .default("center"),
   })
   .describe("Defines silkscreen text on the PCB")
 
