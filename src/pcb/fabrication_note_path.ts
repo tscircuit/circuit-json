d7ceb3454a5cead46c1b295f2028bc96d2026b11
seveibar <seveibar@gmail.com>
2024-06-06 20:55:06 -0700

    add fabrication notes to soup

---
+++ src/pcb/fabrication_note_path.ts
@@ -0,0 +1,18 @@
+import { z } from "zod"
+import { visible_layer } from "./properties/layer_ref"
+import { point } from "src/common"
+
+export const fabrication_note_path = z
+  .object({
+    type: z.literal("fabrication_note_path"),
+    pcb_silkscreen_path_id: z.string(),
+    pcb_component_id: z.string(),
+    layer: visible_layer,
+    route: z.array(point),
+  })
+  .describe(
+    "Defines a fabrication path on the PCB for fabricators or assemblers"
+  )
+
+export type FabricationNotePath = z.infer<typeof fabrication_note_path>
+export type FabricationNotePathInput = z.input<typeof fabrication_note_path>
---
+++ src/pcb/fabrication_note_text.ts
@@ -0,0 +1,21 @@
+import { z } from "zod"
+import { visible_layer } from "./properties/layer_ref"
+import { point } from "src/common"
+import { distance } from "src/units"
+
+export const fabrication_note_text = z
+  .object({
+    type: z.literal("fabrication_note_text"),
+    font: z.literal("tscircuit2024").default("tscircuit2024"),
+    font_size: distance.default("1mm"),
+    pcb_component_id: z.string(),
+    text: z.string(),
+    layer: visible_layer,
+    center: point,
+  })
+  .describe(
+    "Defines a fabrication note in text on the PCB, useful for leaving notes for assemblers or fabricators"
+  )
+
+export type FabricationNoteText = z.infer<typeof fabrication_note_text>
+export type FabricationNoteTextInput = z.input<typeof fabrication_note_text>
