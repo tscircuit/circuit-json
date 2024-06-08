c5157e39cac1b0f9ff7b434b06ba09736a6312fb
seveibar <seveibar@gmail.com>
2024-06-08 12:49:40 -0700

    fix: element soup type for fab not

--- src/pcb/pcb_fabrication_note_text.ts
+++ src/pcb/pcb_fabrication_note_text.ts
@@ -5,7 +5,7 @@ import { distance } from "src/units"
 
 export const pcb_fabrication_note_text = z
   .object({
-    type: z.literal("fabrication_note_text"),
+    type: z.literal("pcb_fabrication_note_text"),
     font: z.literal("tscircuit2024").default("tscircuit2024"),
     font_size: distance.default("1mm"),
     pcb_component_id: z.string(),
