a6fa7d38aa52fd2b449d11bf149bd5e41b3f88ba
seveibar <seveibar@gmail.com>
2024-06-11 21:31:00 -0700

    add support for pcb hole

--- src/pcb/pcb_hole.ts
+++ src/pcb/pcb_hole.ts
@@ -4,10 +4,21 @@ import { distance } from "../units"
 export const pcb_hole = z
   .object({
     type: z.literal("pcb_hole"),
+    hole_shape: z.enum(["round", "square"]).default("round"),
     hole_diameter: z.number(),
     x: distance,
     y: distance,
   })
+  .or(
+    z.object({
+      type: z.literal("pcb_hole"),
+      hole_shape: z.literal("oval"),
+      hole_width: z.number(),
+      hole_height: z.number(),
+      x: distance,
+      y: distance,
+    })
+  )
   .describe("Defines a hole on the PCB")
 
 export type PCBHoleInput = z.input<typeof pcb_hole>
