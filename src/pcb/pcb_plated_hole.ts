69bec99bcb4d9dcfd59c8062a1dadaa12b98ac3c
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2024-06-26 16:49:17 +0530

    feat: Support added for circle and oval

--- src/pcb/pcb_plated_hole.ts
+++ src/pcb/pcb_plated_hole.ts
@@ -5,8 +5,23 @@ import { layer_ref } from "./properties/layer_ref"
 export const pcb_plated_hole = z
   .object({
     type: z.literal("pcb_plated_hole"),
+    shape: z.literal("circle"),
     outer_diameter: z.number(),
     hole_diameter: z.number(),
+    x: distance,
+    y: distance,
+    layers: z.array(layer_ref),
+    port_hints: z.array(z.string()).optional(),
+    pcb_component_id: z.string().optional(),
+    pcb_port_id: z.string().optional(),
+  })
+  .object({
+    type: z.literal("pcb_plated_hole"),
+    shape: z.literal("oval"),
+    outer_width: z.number(),
+    outer_height: z.number(),
+    hole_width: z.number(),
+    hole_height: z.number(),
     x: distance,
     y: distance,
     layers: z.array(layer_ref),
