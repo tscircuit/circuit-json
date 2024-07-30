fb099b09ac7f040bceec3fef8dee9d7eeae74da0
Abhijit Roy <roya51788@gmail.com>
2024-07-31 00:42:42 +0530

    Deprecate source_simple_bug

--- src/source/index.ts
+++ src/source/index.ts
@@ -1,29 +1,15 @@
-import { z } from "zod";
-
-export * from "./source_simple_capacitor";
-export * from "./source_simple_resistor";
-export * from "./source_simple_diode";
-export * from "./source_simple_ground";
-export * from "./source_simple_bug";
-export * from "./source_simple_chip";
-export * from "./source_simple_inductor";
-export * from "./source_led";
-export * from "./source_simple_power_source";
-export * from "./any_source_component";
-export * from "./source_port";
-export * from "./source_trace";
-export * from "./base/source_component_base";
-export * from "./source_group";
-export * from "./source_net";
-
-/**
- * @deprecated Use source_simple_chip instead. This will be removed in a future version.
- */
-export const source_simple_bug = z.object({
-  type: z.literal("source_component"),
-  ftype: z.string().optional(),
-  source_component_id: z.string(),
-  name: z.string(),
-  manufacturer_part_number: z.string().optional(),
-  supplier_part_numbers: z.string().optional(),
-}).describe('@deprecated');
+export * from "./source_simple_capacitor"
+export * from "./source_simple_resistor"
+export * from "./source_simple_diode"
+export * from "./source_simple_ground"
+export * from "./source_simple_bug"
+export * from "./source_simple_chip"
+export * from "./source_simple_inductor"
+export * from "./source_led"
+export * from "./source_simple_power_source"
+export * from "./any_source_component"
+export * from "./source_port"
+export * from "./source_trace"
+export * from "./base/source_component_base"
+export * from "./source_group"
+export * from "./source_net"
--- src/source/source_simple_bug.ts
+++ src/source/source_simple_bug.ts
@@ -1,9 +1,12 @@
 import { source_component_base } from "src/source/base/source_component_base"
 import { z } from "zod"
 
+/**
+ * @deprecated Use source_simple_chip instead. This will be removed in a future version.
+ */
 export const source_simple_bug = source_component_base.extend({
   ftype: z.literal("simple_bug"),
-})
+}).describe('@deprecated')
 
 export type source_simple_bug = z.infer<typeof source_simple_bug>
 export type SourceSimpleBugInput = z.input<typeof source_simple_bug>
