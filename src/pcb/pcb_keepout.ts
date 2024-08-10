c30b9fbea5721877ac99a3265b4ffc54b7aa1e27
Rishabh Gupta <belikerishabh@gmail.com>
2024-08-11 00:05:31 +0530

    refactor

--- src/pcb/pcb_keepout.ts
+++ src/pcb/pcb_keepout.ts
@@ -1,11 +1,13 @@
-import { z } from "zod"
-import { point } from "../common"
-import { distance } from "../units"
+import { z } from "zod";
+import { point } from "../common";
+import { distance } from "../units";
 
 export const pcb_keepout = z
   .object({
     type: z.literal("pcb_keepout"),
     pcb_keepout_id: z.string(),
+    layer: z.array(z.string()).optional(), // Specify layers where the keepout applies
+    description: z.string().optional(), // Optional description of the keepout
   })
   .and(
     z
@@ -21,17 +23,9 @@ export const pcb_keepout = z
           shape: z.literal("circle"),
           center: point,
           radius: distance,
-        }),
-      ),
-  )
-  .and(
-    z
-      .object({
-        layer: z.array(z.string()).optional(), // Specify layers where the keepout applies
-        description: z.string().optional(), // Optional description of the keepout
-      })
-      .optional(),
-  )
+        })
+      )
+  );
 
-export type PCBKeepoutInput = z.input<typeof pcb_keepout>
-export type PCBKeepout = z.infer<typeof pcb_keepout>
+export type PCBKeepoutInput = z.input<typeof pcb_keepout>;
+export type PCBKeepout = z.infer<typeof pcb_keepout>;
