7db90ac83031fd1a3c6a63f727025f8ed3d64743
seveibar <seveibar@gmail.com>
2024-11-07 16:51:34 -0800

    add is_junction

--- src/schematic/schematic_trace.ts
+++ src/schematic/schematic_trace.ts
@@ -1,5 +1,27 @@
 import { z } from "zod"
 import { distance } from "../units"
+import { expectTypesMatch } from "src/utils/expect-types-match"
+
+export interface SchematicTraceEdge {
+  from: {
+    x: number
+    y: number
+  }
+  to: {
+    x: number
+    y: number
+  }
+  is_junction?: boolean
+  from_schematic_port_id?: string
+  to_schematic_port_id?: string
+}
+
+export interface SchematicTrace {
+  type: "schematic_trace"
+  schematic_trace_id: string
+  source_trace_id: string
+  edges: SchematicTraceEdge[]
+}
 
 export const schematic_trace = z.object({
   type: z.literal("schematic_trace"),
@@ -15,6 +37,7 @@ export const schematic_trace = z.object({
         x: z.number(),
         y: z.number(),
       }),
+      is_junction: z.boolean().optional(),
       from_schematic_port_id: z.string().optional(),
       to_schematic_port_id: z.string().optional(),
     }),
@@ -22,4 +45,6 @@ export const schematic_trace = z.object({
 })
 
 export type SchematicTraceInput = z.input<typeof schematic_trace>
-export type SchematicTrace = z.infer<typeof schematic_trace>
+type InferredSchematicTrace = z.infer<typeof schematic_trace>
+
+expectTypesMatch<SchematicTraceInput, InferredSchematicTrace>(true)
