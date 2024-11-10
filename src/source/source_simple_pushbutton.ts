bafd968a75a6a154c83d2c80627bce39e022d57a
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2024-11-10 18:18:39 +0200

    Update source_simple_push_button.ts

--- src/source/source_simple_pushbutton.ts
+++ src/source/source_simple_pushbutton.ts
@@ -2,10 +2,10 @@ import { source_component_base } from "src/source/base/source_component_base"
 import { z } from "zod"
 
 export const source_simple_push_button = source_component_base.extend({
-  ftype: z.literal("simple_pushbutton"),
+  ftype: z.literal("simple_push_button"),
 })
 
-export type SourceSimplePushbutton = z.infer<typeof source_simple_pushbutton>
+export type SourceSimplePushbutton = z.infer<typeof source_simple_push_button>
 export type SourceSimplePushbuttonInput = z.input<
-  typeof source_simple_pushbutton
+  typeof source_simple_push_button
 >
