356f11cdff9d1b49f587fa6749ef33723c58b381
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2024-11-10 18:12:05 +0200

    Update source_simple_pushbutton.ts

--- src/source/source_simple_pushbutton.ts
+++ src/source/source_simple_pushbutton.ts
@@ -1,7 +1,7 @@
 import { source_component_base } from "src/source/base/source_component_base"
 import { z } from "zod"
 
-export const source_simple_pushbutton = source_component_base.extend({
+export const source_simple_push_button = source_component_base.extend({
   ftype: z.literal("simple_pushbutton"),
 })
 
