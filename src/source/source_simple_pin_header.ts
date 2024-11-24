7c57f9a2ce44a6fa7564643c01c26c380bd101c3
Abse <abdaslam13.2.2001@gmail.com>
2024-11-24 13:40:49 +0200

    implemented source_simple_pin_header

---
+++ src/source/source_simple_pin_header.ts
@@ -0,0 +1,12 @@
+import { source_component_base } from "src/source/base/source_component_base"
+import { z } from "zod"
+
+export const source_simple_pin_header = source_component_base.extend({
+  ftype: z.literal("simple_pin_header"),
+  pin_count: z.number(),
+})
+
+export type SourceSimplePinHeader = z.infer<typeof source_simple_pin_header>
+export type SourceSimplePinHeaderInput = z.input<
+  typeof source_simple_pin_header
+>
