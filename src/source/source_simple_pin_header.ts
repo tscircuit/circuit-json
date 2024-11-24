b253119412a546dcdfc983e1535d0d653d9c740b
Abse <abdaslam13.2.2001@gmail.com>
2024-11-24 14:45:40 +0200

    added gender for pin hedders

--- src/source/source_simple_pin_header.ts
+++ src/source/source_simple_pin_header.ts
@@ -4,6 +4,7 @@ import { z } from "zod"
 export const source_simple_pin_header = source_component_base.extend({
   ftype: z.literal("simple_pin_header"),
   pin_count: z.number(),
+  gender: z.enum(["male", "female"]).default("male"),
 })
 
 export type SourceSimplePinHeader = z.infer<typeof source_simple_pin_header>
