017bd0761b6df3814f7eacebebfc1f16b752ac9c
seveibar <seveibar@gmail.com>
2024-12-09 01:57:18 -0800

    add voltage

--- src/schematic/schematic_voltage_probe.ts
+++ src/schematic/schematic_voltage_probe.ts
@@ -1,12 +1,14 @@
 import { z } from "zod"
 import { point, type Point } from "../common/point"
 import { expectTypesMatch } from "src/utils/expect-types-match"
+import { voltage } from "src/units"
 
 export interface SchematicVoltageProbe {
   type: "schematic_voltage_probe"
   schematic_voltage_probe_id: string
   position: Point
   schematic_trace_id: string
+  voltage: number
 }
 
 export const schematic_voltage_probe = z
@@ -15,6 +17,7 @@ export const schematic_voltage_probe = z
     schematic_voltage_probe_id: z.string(),
     position: point,
     schematic_trace_id: z.string(),
+    voltage: voltage,
   })
   .describe("Defines a voltage probe measurement point on a schematic trace")
 
