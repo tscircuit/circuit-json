6f99765d54f22300fd1a2a9dae120e1b8f0bf5b4
seveibar <seveibar@gmail.com>
2024-12-09 15:58:38 -0800

    refactor source elements

--- src/source/source_simple_power_source.ts
+++ src/source/source_simple_power_source.ts
@@ -1,13 +1,29 @@
-import { source_component_base } from "src/source/base/source_component_base"
 import { z } from "zod"
+import {
+  source_component_base,
+  type SourceComponentBase,
+} from "src/source/base/source_component_base"
 import { voltage } from "../units"
+import { expectTypesMatch } from "src/utils/expect-types-match"
 
 export const source_simple_power_source = source_component_base.extend({
   ftype: z.literal("simple_power_source"),
   voltage,
 })
 
-export type SourceSimplePowerSource = z.infer<typeof source_simple_power_source>
 export type SourceSimplePowerSourceInput = z.input<
   typeof source_simple_power_source
 >
+type InferredSourceSimplePowerSource = z.infer<
+  typeof source_simple_power_source
+>
+
+/**
+ * Defines a simple power source component
+ */
+export interface SourceSimplePowerSource extends SourceComponentBase {
+  ftype: "simple_power_source"
+  voltage: number
+}
+
+expectTypesMatch<SourceSimplePowerSource, InferredSourceSimplePowerSource>(true)
