d41e519df75133dae0a6a98f42e16f7467b1cf4f
seveibar <seveibar@gmail.com>
2024-12-09 16:34:02 -0800

    refactor more schematic elements to expect types match pattern

--- src/schematic/schematic_error.ts
+++ src/schematic/schematic_error.ts
@@ -1,9 +1,17 @@
 import { z } from "zod"
+import { expectTypesMatch } from "src/utils/expect-types-match"
+
+export interface SchematicError {
+  type: "schematic_error"
+  schematic_error_id: string
+  error_type: "schematic_port_not_found"
+  message: string
+}
 
 export const schematic_error = z
   .object({
-    schematic_error_id: z.string(),
     type: z.literal("schematic_error"),
+    schematic_error_id: z.string(),
     // eventually each error type should be broken out into a dir of files
     error_type: z.literal("schematic_port_not_found"),
     message: z.string(),
@@ -11,4 +19,6 @@ export const schematic_error = z
   .describe("Defines a schematic error on the schematic")
 
 export type SchematicErrorInput = z.input<typeof schematic_error>
-export type SchematicError = z.infer<typeof schematic_error>
+type InferredSchematicError = z.infer<typeof schematic_error>
+
+expectTypesMatch<SchematicError, InferredSchematicError>(true)
--- src/schematic/schematic_path.ts
+++ src/schematic/schematic_path.ts
@@ -1,5 +1,14 @@
 import { z } from "zod"
-import { point } from "../common/point"
+import { point, type Point } from "../common/point"
+import { expectTypesMatch } from "src/utils/expect-types-match"
+
+export interface SchematicPath {
+  type: "schematic_path"
+  schematic_component_id: string
+  fill_color?: "red" | "blue"
+  is_filled?: boolean
+  points: Point[]
+}
 
 export const schematic_path = z.object({
   type: z.literal("schematic_path"),
@@ -10,4 +19,6 @@ export const schematic_path = z.object({
 })
 
 export type SchematicPathInput = z.input<typeof schematic_path>
-export type SchematicPath = z.infer<typeof schematic_path>
+type InferredSchematicPath = z.infer<typeof schematic_path>
+
+expectTypesMatch<SchematicPath, InferredSchematicPath>(true)
