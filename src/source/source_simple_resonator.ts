a3c0e010f7e0ffa5afd968a6e7cfc10aa0739acf
Anshgrover23 <ag5989670@gmail.com>
2024-12-20 21:26:57 +0530

    typo in ftype

--- src/source/source_simple_resonator.ts
+++ src/source/source_simple_resonator.ts
@@ -2,7 +2,7 @@ import { source_component_base } from "src/source/base/source_component_base"
 import { z } from "zod"
 
 export const source_simple_resonator = source_component_base.extend({
-  ftype: z.literal("resonator"),
+  ftype: z.literal("simple_resonator"),
   frequency: z.union([z.string(), z.number()]),
   load_capacitance: z.union([z.string(), z.number()]),
   pin_count: z.number().default(3), // Set the default pin count to 3 for resonators
