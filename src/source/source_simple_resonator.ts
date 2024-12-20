f2c02310a797a10c11f91f666f1c70ac132ff019
Anshgrover23 <ag5989670@gmail.com>
2024-12-21 00:08:03 +0530

    frequency

--- src/source/source_simple_resonator.ts
+++ src/source/source_simple_resonator.ts
@@ -1,21 +1,10 @@
 import { source_component_base } from "src/source/base/source_component_base"
 import { z } from "zod"
-import { parseAndConvertSiUnit } from "src/utils/convert-si-unit-to-number"
-
-const parseFrequency = z.union([z.string(), z.number()]).transform((val) => {
-  if (typeof val === "string" || typeof val === "number") {
-    const { value, unitOfValue } = parseAndConvertSiUnit(val)
-    if (unitOfValue !== "Hz") {
-      throw new Error(`Invalid frequency unit: ${unitOfValue}`)
-    }
-    return value
-  }
-  throw new Error("Invalid frequency format")
-})
+import { frequency } from "src/units"
 
 export const source_simple_resonator = source_component_base.extend({
   ftype: z.literal("simple_resonator"),
-  frequency: parseFrequency,
+  frequency,
   load_capacitance: z.union([z.string(), z.number()]),
   pin_count: z.number().default(3), // Set the default pin count to 3 for resonators
 })
