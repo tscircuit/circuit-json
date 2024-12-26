59efb90bd84a145240026ebe32d1aa0ce252587c
Anshgrover23 <ag5989670@gmail.com>
2024-12-27 00:25:30 +0530

    fix: source_simple_transistor json file

--- src/source/source_simple_transistor.ts
+++ src/source/source_simple_transistor.ts
@@ -3,24 +3,11 @@ import {
   source_component_base,
   type SourceComponentBase,
 } from "src/source/base/source_component_base"
-import { voltage, current } from "src/units"
 import { expectTypesMatch } from "src/utils/expect-types-match"
-import {
-  schematic_port,
-  type SchematicPort,
-} from "src/schematic/schematic_port"
 
 export const source_simple_transistor = source_component_base.extend({
   ftype: z.literal("simple_transistor"),
   transistortype: z.enum(["npn", "pnp"]),
-  schematic_ports: z.object({
-    collector: schematic_port,
-    base: schematic_port,
-    emitter: schematic_port,
-  }),
-  collectorEmitterVoltage: voltage.optional(),
-  baseEmitterVoltage: voltage.optional(),
-  collectorCurrent: current.optional(),
 })
 
 export type SourceSimpleTransistorInput = z.input<
@@ -37,14 +24,6 @@ type InferredSourceSimpleTransistor = z.infer<typeof source_simple_transistor>
 export interface SourceSimpleTransistor extends SourceComponentBase {
   ftype: "simple_transistor"
   transistortype: "npn" | "pnp"
-  schematic_ports: {
-    collector: SchematicPort
-    base: SchematicPort
-    emitter: SchematicPort
-  }
-  collectorEmitterVoltage?: number
-  baseEmitterVoltage?: number
-  collectorCurrent?: number
 }
 
 expectTypesMatch<SourceSimpleTransistor, InferredSourceSimpleTransistor>(true)
