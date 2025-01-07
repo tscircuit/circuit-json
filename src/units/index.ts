8a897cb1f4a00a26704e0374786bd36e211633c3
seveibar <seveibar@gmail.com>
2025-01-07 10:44:16 -0800

    better test

--- src/units/index.ts
+++ src/units/index.ts
@@ -63,7 +63,7 @@ export const capacitance = z
   .or(z.number())
   .transform((v) => parseAndConvertSiUnit(v).value!)
   .transform((value) => {
-    return parseFloat(value.toPrecision(12)) // Round to 12 significant digits
+    return Number.parseFloat(value.toPrecision(12)) // Round to 12 significant digits
   })
 
 export const inductance = z
