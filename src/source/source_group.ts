6d8e2f25a248a41123235390ffa018bb79056763
Severin Ibarluzea <seveibar@gmail.com>
2025-01-22 09:36:46 -0800

    add parent subcircuit id

--- src/source/source_group.ts
+++ src/source/source_group.ts
@@ -4,6 +4,7 @@ export const source_group = z.object({
   type: z.literal("source_group"),
   source_group_id: z.string(),
   subcircuit_id: z.string().optional(),
+  parent_subcircuit_id: z.string().optional(),
   is_subcircuit: z.boolean().optional(),
   name: z.string().optional(),
 })
