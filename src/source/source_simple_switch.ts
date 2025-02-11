58d2bb9c173b596782e73f963596db3c9b0a25bd
techmannih <manishchaudharyttt@gmail.com>
2025-02-11 23:11:13 +0530

    format code

--- src/source/source_simple_switch.ts
+++ src/source/source_simple_switch.ts
@@ -1,21 +1,21 @@
-import { z } from "zod";
+import { z } from "zod"
 import {
   source_component_base,
   type SourceComponentBase,
-} from "src/source/base/source_component_base";
-import { expectTypesMatch } from "src/utils/expect-types-match";
+} from "src/source/base/source_component_base"
+import { expectTypesMatch } from "src/utils/expect-types-match"
 
 export const source_simple_switch = source_component_base.extend({
   ftype: z.literal("simple_switch"),
 })
-export type SourceSimpleSwitchInput = z.input<typeof source_simple_switch>;
-type InferredSourceSimpleSwitch = z.infer<typeof source_simple_switch>;
+export type SourceSimpleSwitchInput = z.input<typeof source_simple_switch>
+type InferredSourceSimpleSwitch = z.infer<typeof source_simple_switch>
 
 /**
  * Defines a simple switch component
  */
 export interface SourceSimpleSwitch extends SourceComponentBase {
-  ftype: "simple_switch";
+  ftype: "simple_switch"
 }
 
-expectTypesMatch<SourceSimpleSwitch, InferredSourceSimpleSwitch>(true);
+expectTypesMatch<SourceSimpleSwitch, InferredSourceSimpleSwitch>(true)
