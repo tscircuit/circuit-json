6faccc6301a21aeb664aa9f2109efa2d33551750
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2025-02-12 15:26:29 +0530

    add switch src (#131)

--- src/any_circuit_element.ts
+++ src/any_circuit_element.ts
@@ -25,6 +25,7 @@ export const any_circuit_element = z.union([
   src.source_simple_inductor,
   src.source_simple_pin_header,
   src.source_simple_resonator,
+  src.source_simple_switch,
   src.source_simple_transistor,
   src.source_simple_mosfet,
   src.source_simple_potentiometer,
