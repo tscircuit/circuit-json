a7d053ed73652ba95a3dabd2da2dadfcc05e95ed
Ansh Grover <168731971+Anshgrover23@users.noreply.github.com>
2025-02-15 15:48:50 +0530

    add maxVoltageRating to capacitor (#133)
    * add maxVoltageRating to capacitor
    
    * add maxVoltageRating to capacitor

--- src/source/source_simple_capacitor.ts
+++ src/source/source_simple_capacitor.ts
@@ -3,12 +3,13 @@ import {
   source_component_base,
   type SourceComponentBase,
 } from "src/source/base/source_component_base"
-import { capacitance, distance } from "src/units"
+import { capacitance, distance, voltage } from "src/units"
 import { expectTypesMatch } from "src/utils/expect-types-match"
 
 export const source_simple_capacitor = source_component_base.extend({
   ftype: z.literal("simple_capacitor"),
   capacitance,
+  max_voltage_rating: voltage.optional(),
   display_capacitance: z.string().optional(),
   max_decoupling_trace_length: distance.optional(),
 })
@@ -22,6 +23,7 @@ type InferredSourceSimpleCapacitor = z.infer<typeof source_simple_capacitor>
 export interface SourceSimpleCapacitor extends SourceComponentBase {
   ftype: "simple_capacitor"
   capacitance: number
+  max_voltage_rating?: number
   display_capacitance?: string
   max_decoupling_trace_length?: number
 }
