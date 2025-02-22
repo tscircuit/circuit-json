87361b225caf61c7c3e0a9077b7507d0644f6fb2
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-02-22 22:28:52 +0200

    adding the autorouting_error type to any_circuit_element (#138)
    * adding the autorouting type to any_circuit_element
    
    * following the convention

--- src/pcb/pcb_autorouting_error.ts
+++ src/pcb/pcb_autorouting_error.ts
@@ -2,23 +2,23 @@ import { z } from "zod"
 import { getZodPrefixedIdWithDefault } from "src/common"
 import { expectTypesMatch } from "src/utils/expect-types-match"
 
-export const autorouting_error = z
+export const pcb_autorouting_error = z
   .object({
-    type: z.literal("autorouting_error"),
-    pcb_error_id: getZodPrefixedIdWithDefault("autorouting_error"),
+    type: z.literal("pcb_autorouting_error"),
+    pcb_error_id: getZodPrefixedIdWithDefault("pcb_autorouting_error"),
     message: z.string(),
   })
   .describe("The autorouting has failed to route a portion of the board")
 
-export type AutoroutingErrorInput = z.input<typeof autorouting_error>
-type InferredAutoroutingError = z.infer<typeof autorouting_error>
+export type PcbAutoroutingErrorInput = z.input<typeof pcb_autorouting_error>
+type PcbInferredAutoroutingError = z.infer<typeof pcb_autorouting_error>
 
-export interface AutoroutingErrorInterface {
-  type: "autorouting_error"
+export interface PcbAutoroutingErrorInterface {
+  type: "pcb_autorouting_error"
   pcb_error_id: string
   message: string
 }
 
-export type AutoroutingError = AutoroutingErrorInterface
+export type PcbAutoroutingError = PcbAutoroutingErrorInterface
 
-expectTypesMatch<AutoroutingError, InferredAutoroutingError>(true)
+expectTypesMatch<PcbAutoroutingError, PcbInferredAutoroutingError>(true)
