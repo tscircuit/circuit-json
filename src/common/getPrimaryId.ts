a441accd31eb0eb3fa6e1a1af17a4f07b3bbb187
Anshgrover23 <ag5989670@gmail.com>
2025-03-01 23:15:51 +0530

    add getPrimaryId utility function

--- src/common/getPrimaryId.ts
+++ src/common/getPrimaryId.ts
@@ -2,15 +2,9 @@ import { z } from "zod"
 import { nanoid } from "nanoid"
 
 /**
- * Returns the primary id for any element.
+ * Returns the primary id schema for any element.
  * Automatically typechecked to ensure proper handling of new elements.
  */
-export const getPrimaryId = (prefix: string): string => {
-  const idSchema = z.string().startsWith(prefix)
-  const id = `${prefix}_${nanoid()}`
-
-  // Validate the generated ID
-  idSchema.parse(id)
-
-  return id
+export const getPrimaryId = (prefix: string) => {
+  return z.string().default(() => `${prefix}_${nanoid()}`)
 }
