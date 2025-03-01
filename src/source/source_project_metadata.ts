7ca7aa67eb3e19adb971a102b4ca51a5d84f2ec6
Severin Ibarluzea <seveibar@gmail.com>
2025-03-01 15:10:46 -0800

    Fix source project metadata exports (#158)
    * fix more source project metadata stuff
    
    * rename type

--- src/source/source_project_metadata.ts
+++ src/source/source_project_metadata.ts
@@ -1,17 +1,19 @@
+import { expectTypesMatch } from "src/utils/expect-types-match"
 import { z } from "zod"
 
-export interface ProjectMetadata {
+export interface SourceProjectMetadata {
   type: "source_project_metadata"
   name?: string
   software_used_string?: string
   created_at?: string // ISO8601 timestamp
 }
 
-export const project_metadata = z.object({
+export const source_project_metadata = z.object({
   type: z.literal("source_project_metadata"),
   name: z.string().optional(),
   software_used_string: z.string().optional(),
   created_at: z.string().datetime().optional(),
 })
 
-export type InferredProjectMetadata = z.infer<typeof project_metadata>
+export type InferredProjectMetadata = z.infer<typeof source_project_metadata>
+expectTypesMatch<SourceProjectMetadata, InferredProjectMetadata>(true)
