33b87797c9f2a98db6938eeede3115bbda5d6723
techmannih <manishchaudharyttt@gmail.com>
2025-03-04 00:37:14 +0530

    add support pill shape hole with rect pad

--- src/pcb/pcb_plated_hole.ts
+++ src/pcb/pcb_plated_hole.ts
@@ -96,6 +96,44 @@ const pcb_circular_hole_with_rect_pad = z.object({
   pcb_port_id: z.string().optional(),
   pcb_plated_hole_id: getZodPrefixedIdWithDefault("pcb_plated_hole"),
 })
+const pcb_pill_hole_with_rect_pad = z.object({
+  type: z.literal("pcb_plated_hole"),
+  shape: z.literal("pill_hole_with_rect_pad"),
+  pcb_group_id: z.string().optional(),
+  subcircuit_id: z.string().optional(),
+  hole_shape: z.literal("pill"),
+  pad_shape: z.literal("rect"),
+  hole_width: z.number(),
+  hole_height: z.number(),
+  rect_pad_width: z.number(),
+  rect_pad_height: z.number(),
+  x: distance,
+  y: distance,
+  layers: z.array(layer_ref),
+  port_hints: z.array(z.string()).optional(),
+  pcb_component_id: z.string().optional(),
+  pcb_port_id: z.string().optional(),
+  pcb_plated_hole_id: getZodPrefixedIdWithDefault("pcb_plated_hole"),
+})
+export interface PcbHolePillWithRectPad {
+  type: "pcb_plated_hole"
+  shape: "pill_hole_with_rect_pad"
+  pcb_group_id?: string
+  subcircuit_id?: string
+  hole_shape: "pill"
+  pad_shape: "rect"
+  hole_width: number
+  hole_height: number
+  rect_pad_width: number
+  rect_pad_height: number
+  x: Distance
+  y: Distance
+  layers: LayerRef[]
+  port_hints?: string[]
+  pcb_component_id?: string
+  pcb_port_id?: string
+  pcb_plated_hole_id: string
+}
 
 export interface PcbHoleCircularWithRectPad {
   type: "pcb_plated_hole"
@@ -125,6 +163,7 @@ export type PcbPlatedHole =
   | PcbPlatedHoleCircle
   | PcbPlatedHoleOval
   | PcbHoleCircularWithRectPad
+  | PcbHolePillWithRectPad
 
 expectTypesMatch<PcbPlatedHoleCircle, z.infer<typeof pcb_plated_hole_circle>>(
   true,
