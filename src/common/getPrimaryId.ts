9e3b961d88d06f4dc199c3c24e8f37ebde1c0770
techmannih <manishchaudharyttt@gmail.com>
2025-03-15 18:16:21 +0530

    add getprimaryid

--- src/common/getPrimaryId.ts
+++ src/common/getPrimaryId.ts
@@ -1,9 +1,127 @@
+type ExpectedElementTypes =
+  | "source_trace"
+  | "source_port"
+  | "source_component"
+  | "source_net"
+  | "source_group"
+  | "pcb_component"
+  | "pcb_hole"
+  | "pcb_missing_footprint_error"
+  | "pcb_manual_edit_conflict_error"
+  | "pcb_plated_hole"
+  | "pcb_keepout"
+  | "pcb_port"
+  | "pcb_text"
+  | "pcb_trace"
+  | "pcb_via"
+  | "pcb_smtpad"
+  | "pcb_solder_paste"
+  | "pcb_board"
+  | "pcb_group"
+  | "pcb_trace_hint"
+  | "pcb_silkscreen_line"
+  | "pcb_silkscreen_path"
+  | "pcb_silkscreen_text"
+  | "pcb_silkscreen_rect"
+  | "pcb_silkscreen_circle"
+  | "pcb_silkscreen_oval"
+  | "pcb_trace_error"
+  | "pcb_placement_error"
+  | "pcb_port_not_matched_error"
+  | "pcb_fabrication_note_path"
+  | "pcb_fabrication_note_text"
+  | "pcb_autorouting_error"
+  | "schematic_box"
+  | "schematic_text"
+  | "schematic_line"
+  | "schematic_component"
+  | "schematic_port"
+  | "schematic_trace"
+  | "schematic_path"
+  | "schematic_error"
+  | "schematic_net_label"
+  | "schematic_debug_object"
+  | "schematic_voltage_probe"
+  | "cad_component"
+
+/**
+ * Function to get the primary ID of an element based on its type.
+ * It checks for the correct field and suffix in the ID based on the element's type.
+ * @param element - The circuit element object to validate.
+ * @returns The ID string if found, otherwise a string indicating the error.
+ */
 export function getPrimaryId(element: any): string | undefined {
-  // Return the first property ending with _id that is a string.
-  for (const key in element) {
-    if (/_id$/.test(key) && typeof element[key] === "string") {
-      return element[key]
-    }
+  if (!isValidType(element.type)) {
+    return `Invalid primaryId for type: '${element.type}'. Expected one of the valid types.` // Improved error message
   }
-  return undefined
+
+  const expectedField = `${element.type}_id`
+
+  if (element[expectedField]) {
+    return element[expectedField]
+  }
+
+  const incorrectFieldName = Object.keys(element).find(
+    (key) => key !== expectedField && key.endsWith("_id"),
+  )
+  if (incorrectFieldName) {
+    return `Invalid primaryId for type: '${element.type}'. Expected primaryId to include '${expectedField}' instead of '${incorrectFieldName}'`
+  }
+
+  return `Invalid primaryId for type: '${element.type}'. Expected primaryId to include '${expectedField}'.`
+}
+
+/**
+ * Validate if the element's type is valid.
+ * @param type - The type of the circuit element to validate.
+ * @returns true if the type is valid, otherwise false.
+ */
+function isValidType(type: any): type is ExpectedElementTypes {
+  console.log("Checking type:", type) // Add a log to see what type is being checked
+  return [
+    "source_trace",
+    "source_port",
+    "source_component",
+    "source_net",
+    "source_group",
+    "pcb_component",
+    "pcb_hole",
+    "pcb_missing_footprint_error",
+    "pcb_manual_edit_conflict_error",
+    "pcb_plated_hole",
+    "pcb_keepout",
+    "pcb_port",
+    "pcb_text",
+    "pcb_trace",
+    "pcb_via",
+    "pcb_smtpad",
+    "pcb_solder_paste",
+    "pcb_board",
+    "pcb_group",
+    "pcb_trace_hint",
+    "pcb_silkscreen_line",
+    "pcb_silkscreen_path",
+    "pcb_silkscreen_text",
+    "pcb_silkscreen_rect",
+    "pcb_silkscreen_circle",
+    "pcb_silkscreen_oval",
+    "pcb_trace_error",
+    "pcb_placement_error",
+    "pcb_port_not_matched_error",
+    "pcb_fabrication_note_path",
+    "pcb_fabrication_note_text",
+    "pcb_autorouting_error",
+    "schematic_box",
+    "schematic_text",
+    "schematic_line",
+    "schematic_component",
+    "schematic_port",
+    "schematic_trace",
+    "schematic_path",
+    "schematic_error",
+    "schematic_net_label",
+    "schematic_debug_object",
+    "schematic_voltage_probe",
+    "cad_component",
+  ].includes(type)
 }
---
+++ tests/get-primary-id.test.ts
@@ -0,0 +1,72 @@
+import { expect, test } from "bun:test"
+import { getPrimaryId } from "src/common/getPrimaryId"
+
+test("getPrimaryId - Valid ID for pcb_smtpad", () => {
+  const element = { type: "pcb_smtpad", pcb_smtpad_id: "pcb_smtpad_id_123" }
+  expect(getPrimaryId(element)).toEqual("pcb_smtpad_id_123")
+})
+
+test("getPrimaryId - Valid ID for pcb_smtpad (alternative)", () => {
+  const element = { type: "pcb_smtpad", pcb_smtpad_id: "invalid_id_123" }
+  expect(getPrimaryId(element)).toEqual("invalid_id_123")
+})
+
+test("getPrimaryId - Valid ID for pcb_component", () => {
+  const element = {
+    type: "pcb_component",
+    pcb_component_id: "pcb_component_id_456",
+  }
+  expect(getPrimaryId(element)).toEqual("pcb_component_id_456")
+})
+
+test("getPrimaryId - Valid ID for pcb_component (alternative)", () => {
+  const element = {
+    type: "pcb_component",
+    pcb_component_id: "wrong_component_id_789",
+  }
+  expect(getPrimaryId(element)).toEqual("wrong_component_id_789")
+})
+
+test("getPrimaryId - Valid ID for schematic_box", () => {
+  const element = {
+    type: "schematic_box",
+    schematic_box_id: "schematic_box_id_101",
+  }
+  expect(getPrimaryId(element)).toEqual("schematic_box_id_101")
+})
+
+test("getPrimaryId - Valid ID for schematic_box (alternative)", () => {
+  const element = {
+    type: "schematic_box",
+    schematic_box_id: "wrong_schematic_box_id_101",
+  }
+  expect(getPrimaryId(element)).toEqual("wrong_schematic_box_id_101")
+})
+
+test("getPrimaryId - Missing ID", () => {
+  const element = { type: "pcb_smtpad" }
+  expect(getPrimaryId(element)).toEqual(
+    "Invalid primaryId for type: 'pcb_smtpad'. Expected primaryId to include 'pcb_smtpad_id'.",
+  )
+})
+
+test("getPrimaryId - Invalid test for ID field name", () => {
+  const element = { type: "pcb_smdpad", id: "pcb_smdpad_id_10" }
+  expect(getPrimaryId(element)).toEqual(
+    "Invalid primaryId for type: 'pcb_smdpad'. Expected one of the valid types.",
+  )
+})
+
+test("getPrimaryId - Invalid test for incorrect field name", () => {
+  const element = { type: "pcb_smdpad", pcb_id: "pcb_smdpad_id_120" }
+  expect(getPrimaryId(element)).toEqual(
+    "Invalid primaryId for type: 'pcb_smdpad'. Expected one of the valid types.",
+  )
+})
+
+test("getPrimaryId - Invalid test for wrong ID field name", () => {
+  const element = { type: "pcb_smt", smt_id: "pcb_smt_id_10" }
+  expect(getPrimaryId(element)).toEqual(
+    "Invalid primaryId for type: 'pcb_smt'. Expected one of the valid types.",
+  )
+})
