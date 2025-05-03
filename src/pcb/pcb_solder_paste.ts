e9bcaaf786a72175c86a53bd40740d4b170e3798
techmannih <manishchaudharyttt@gmail.com>
2025-05-03 11:02:18 +0530

    add support all pcb solder paste for plated hole

--- src/pcb/pcb_solder_paste.ts
+++ src/pcb/pcb_solder_paste.ts
@@ -62,29 +62,91 @@ const pcb_solder_paste_rotated_rect = z.object({
   pcb_component_id: z.string().optional(),
   pcb_smtpad_id: z.string().optional(),
 })
-
-const pcb_solder_paste_oval = z.object({
+//
+const pcb_solder_paste_circular_plated_hole = z.object({
   type: z.literal("pcb_solder_paste"),
-  shape: z.literal("oval"),
+  shape: z.literal("circular_plated_hole"),
+  pcb_solder_paste_id: getZodPrefixedIdWithDefault("pcb_solder_paste"),
+  pcb_plated_hole_id: z.string().optional(),
+  pcb_group_id: z.string().optional(),
+  subcircuit_id: z.string().optional(),
+  hole_diameter: z.number(),
+  outer_diameter: z.number(),
+  x: distance,
+  y: distance,
+  layers: z.array(layer_ref),
+  port_hints: z.array(z.string()).optional(),
+  pcb_component_id: z.string().optional(),
+  pcb_port_id: z.string().optional(),
+})
+const pcb_solder_paste_pill_plated_hole = z.object({
+  type: z.literal("pcb_solder_paste"),
+  shape: z.literal("pill_plated_hole"),
+  pcb_solder_paste_id: getZodPrefixedIdWithDefault("pcb_solder_paste"),
+  pcb_plated_hole_id: z.string().optional(),
+  pcb_group_id: z.string().optional(),
+  subcircuit_id: z.string().optional(),
+  hole_width: z.number(),
+  hole_height: z.number(),
+  outer_width: z.number(),
+  outer_height: z.number(),
+  x: distance,
+  y: distance,
+  layers: z.array(layer_ref),
+  port_hints: z.array(z.string()).optional(),
+  pcb_component_id: z.string().optional(),
+  pcb_port_id: z.string().optional(),
+})
+// circular_hole_with_rect_pad
+const pcb_solder_paste_circular_hole_with_rect_pad = z.object({
+  type: z.literal("pcb_solder_paste"),
+  shape: z.literal("circular_hole_with_rect_pad"),
   pcb_solder_paste_id: getZodPrefixedIdWithDefault("pcb_solder_paste"),
   pcb_group_id: z.string().optional(),
   subcircuit_id: z.string().optional(),
+  hole_shape: z.literal("circle"),
+  pad_shape: z.literal("rect"),
+  hole_diameter: z.number(),
+  rect_pad_width: z.number(),
+  rect_pad_height: z.number(),
   x: distance,
   y: distance,
-  width: z.number(),
-  height: z.number(),
-  layer: layer_ref,
+  layers: z.array(layer_ref),
+  port_hints: z.array(z.string()).optional(),
   pcb_component_id: z.string().optional(),
-  pcb_smtpad_id: z.string().optional(),
+  pcb_port_id: z.string().optional(),
+  pcb_plated_hole_id: z.string().optional(),
 })
-
+const pcb_solder_paste_pill_hole_with_rect_pad = z.object({
+  type: z.literal("pcb_solder_paste"),
+  shape: z.literal("pill_hole_with_rect_pad"),
+  pcb_solder_paste_id: getZodPrefixedIdWithDefault("pcb_solder_paste"),
+  pcb_group_id: z.string().optional(),
+  subcircuit_id: z.string().optional(),
+  hole_shape: z.literal("pill"),
+  pad_shape: z.literal("rect"),
+  hole_width: z.number(),
+  hole_height: z.number(),
+  rect_pad_width: z.number(),
+  rect_pad_height: z.number(),
+  x: distance,
+  y: distance,
+  layers: z.array(layer_ref),
+  port_hints: z.array(z.string()).optional(),
+  pcb_component_id: z.string().optional(),
+  pcb_port_id: z.string().optional(),
+  pcb_plated_hole_id: z.string().optional(),
+})
 export const pcb_solder_paste = z
   .union([
     pcb_solder_paste_circle,
     pcb_solder_paste_rect,
     pcb_solder_paste_pill,
     pcb_solder_paste_rotated_rect,
-    pcb_solder_paste_oval,
+    pcb_solder_paste_circular_plated_hole,
+    pcb_solder_paste_pill_plated_hole,
+    pcb_solder_paste_circular_hole_with_rect_pad,
+    pcb_solder_paste_pill_hole_with_rect_pad,
   ])
   .describe("Defines solderpaste on the PCB")
 
@@ -95,7 +157,18 @@ type InferredPcbSolderPastePill = z.infer<typeof pcb_solder_paste_pill>
 type InferredPcbSolderPasteRotatedRect = z.infer<
   typeof pcb_solder_paste_rotated_rect
 >
-type InferredPcbSolderPasteOval = z.infer<typeof pcb_solder_paste_oval>
+type InferredPcbSolderPasteCircularPlatedHole = z.infer<
+  typeof pcb_solder_paste_circular_plated_hole
+>
+type InferredPcbSolderPastePillPlatedHole = z.infer<
+  typeof pcb_solder_paste_pill_plated_hole
+>
+type InferredPcbSolderPasteCircularHoleWithRectPad = z.infer<
+  typeof pcb_solder_paste_circular_hole_with_rect_pad
+>
+type InferredPcbSolderPastePillHoleWithRectPad = z.infer<
+  typeof pcb_solder_paste_pill_hole_with_rect_pad
+>
 
 /**
  * Defines solderpaste on the PCB
@@ -170,32 +243,122 @@ export interface PcbSolderPasteRotatedRect {
 /**
  * Defines solderpaste on the PCB
  */
-export interface PcbSolderPasteOval {
+export interface PcbSolderPasteCircularPlatedHole {
   type: "pcb_solder_paste"
-  shape: "oval"
+  shape: "circular_plated_hole"
   pcb_solder_paste_id: string
   pcb_group_id?: string
   subcircuit_id?: string
+  hole_diameter: number
+  outer_diameter: number
   x: Distance
   y: Distance
-  width: number
-  height: number
-  layer: LayerRef
+  layers: LayerRef[]
+  port_hints?: string[]
   pcb_component_id?: string
-  pcb_smtpad_id?: string
+  pcb_port_id?: string
+  pcb_plated_hole_id?: string
 }
+/**
+ * Defines solderpaste on the PCB
+ */
+export interface PcbSolderPastePillPlatedHole {
+  type: "pcb_solder_paste"
+  shape: "pill_plated_hole"
+  pcb_solder_paste_id: string
+  pcb_group_id?: string
+  subcircuit_id?: string
+  hole_width: number
+  hole_height: number
+  outer_width: number
+  outer_height: number
+  x: Distance
+  y: Distance
+  layers: LayerRef[]
+  port_hints?: string[]
+  pcb_component_id?: string
+  pcb_port_id?: string
+  pcb_plated_hole_id?: string
+}
+/**
+ * Defines solderpaste on the PCB
+ */
+export interface PcbSolderPasteCircularHoleWithRectPad {
+  type: "pcb_solder_paste"
+  shape: "circular_hole_with_rect_pad"
+  pcb_solder_paste_id: string
+  pcb_group_id?: string
+  subcircuit_id?: string
+  hole_shape: "circle"
+  pad_shape: "rect"
+  hole_diameter: number
+  rect_pad_width: number
+  rect_pad_height: number
+  x: Distance
+  y: Distance
+  layers: LayerRef[]
+  port_hints?: string[]
+  pcb_component_id?: string
+  pcb_port_id?: string
+  pcb_plated_hole_id?: string
+}
+
+/**
+ * Defines solderpaste on the PCB
+ */
+export interface PcbSolderPastePillHoleWithRectPad {
+  type: "pcb_solder_paste"
+  shape: "pill_hole_with_rect_pad"
+  pcb_solder_paste_id: string
+  pcb_group_id?: string
+  subcircuit_id?: string
+  hole_shape: "pill"
+  pad_shape: "rect"
+  hole_width: number
+  hole_height: number
+  rect_pad_width: number
+  rect_pad_height: number
+  x: Distance
+  y: Distance
+  layers: LayerRef[]
+  port_hints?: string[]
+  pcb_component_id?: string
+  pcb_port_id?: string
+  pcb_plated_hole_id?: string
+}
+
+/**
+ * Defines solderpaste on the PCB
+ */
 
 export type PcbSolderPaste =
   | PcbSolderPasteCircle
   | PcbSolderPasteRect
   | PcbSolderPastePill
   | PcbSolderPasteRotatedRect
-  | PcbSolderPasteOval
-
+  | PcbSolderPasteCircularPlatedHole
+  | PcbSolderPastePillPlatedHole
+  | PcbSolderPasteCircularHoleWithRectPad
+  | PcbSolderPastePillHoleWithRectPad
 expectTypesMatch<PcbSolderPasteCircle, InferredPcbSolderPasteCircle>(true)
 expectTypesMatch<PcbSolderPasteRect, InferredPcbSolderPasteRect>(true)
 expectTypesMatch<PcbSolderPastePill, InferredPcbSolderPastePill>(true)
 expectTypesMatch<PcbSolderPasteRotatedRect, InferredPcbSolderPasteRotatedRect>(
   true,
 )
-expectTypesMatch<PcbSolderPasteOval, InferredPcbSolderPasteOval>(true)
+expectTypesMatch<
+  PcbSolderPasteCircularPlatedHole,
+  InferredPcbSolderPasteCircularPlatedHole
+>(true)
+expectTypesMatch<
+  PcbSolderPastePillPlatedHole,
+  InferredPcbSolderPastePillPlatedHole
+>(true)
+expectTypesMatch<
+  PcbSolderPasteCircularHoleWithRectPad,
+  InferredPcbSolderPasteCircularHoleWithRectPad
+>(true)
+expectTypesMatch<
+  PcbSolderPastePillHoleWithRectPad,
+  InferredPcbSolderPastePillHoleWithRectPad
+>(true)
