c72ffbd563344c7b24acab728afa09077ca6f7f5
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-05-09 20:14:47 +0530

    Add the location of the missing component (#184)
    * Add the location of the missing component
    
    * formatbot: Automatically format code
    
    * remove source_component_id
    
    * type fix
    
    ---------
    
    Co-authored-by: tscircuitbot <tscircuitbot@users.noreply.github.com>

--- src/source/source_failed_to_create_component_error.ts
+++ src/source/source_failed_to_create_component_error.ts
@@ -11,8 +11,19 @@ export const source_failed_to_create_component_error = z
     component_name: z.string().optional(),
     subcircuit_id: z.string().optional(),
     parent_source_component_id: z.string().optional(),
-    source_component_id: z.string(),
     message: z.string(),
+    pcb_center: z
+      .object({
+        x: z.number().optional(),
+        y: z.number().optional(),
+      })
+      .optional(),
+    schematic_center: z
+      .object({
+        x: z.number().optional(),
+        y: z.number().optional(),
+      })
+      .optional(),
   })
   .describe("Error emitted when a component fails to be constructed")
 
@@ -30,11 +41,18 @@ type InferredSourceFailedToCreateComponentError = z.infer<
 export interface SourceFailedToCreateComponentError {
   type: "source_failed_to_create_component_error"
   source_failed_to_create_component_error_id: string
-  source_component_id: string
   message: string
   component_name?: string
   subcircuit_id?: string
   parent_source_component_id?: string
+  pcb_center?: {
+    x?: number
+    y?: number
+  }
+  schematic_center?: {
+    x?: number
+    y?: number
+  }
 }
 
 expectTypesMatch<
