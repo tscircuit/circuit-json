bc4c3dfa74c816cde39cc59a5cbd21126bbc9f99
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-05-21 15:50:38 +0200

    New component schematic_section

---
+++ src/schematic/schematic_section.ts
@@ -0,0 +1,30 @@
+import { z } from "zod"
+import { distance } from "../units"
+import { expectTypesMatch } from "src/utils/expect-types-match"
+
+export interface SchematicSection {
+  type: "schematic_section"
+  schematic_component_id?: string
+  schematic_section_id: string
+  position: {
+    x: number
+    y: number
+  }
+  rotation: number
+}
+
+export const schematic_section = z.object({
+  type: z.literal("schematic_section"),
+  schematic_component_id: z.string().optional(),
+  schematic_section_id: z.string(),
+  position: z.object({
+    x: distance,
+    y: distance,
+  }),
+  rotation: z.number().default(0),
+})
+
+export type SchematicTextInput = z.input<typeof schematic_section>
+type InferredSchematicText = z.infer<typeof schematic_section>
+
+expectTypesMatch<SchematicSection, InferredSchematicText>(true)
