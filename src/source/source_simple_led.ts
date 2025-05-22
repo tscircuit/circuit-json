73929e243088c44bfa6e75d0a6420be582bc2570
techmannih <manishchaudharyttt@gmail.com>
2025-05-22 20:46:34 +0530

    add

--- src/source/source_simple_led.ts
+++ src/source/source_simple_led.ts
@@ -4,7 +4,10 @@ import {
 } from "src/source/base/source_component_base"
 import { z } from "zod"
 import { expectTypesMatch } from "src/utils/expect-types-match"
-import { source_simple_diode } from "./source_simple_diode"
+import {
+  source_simple_diode,
+  type SourceSimpleDiode,
+} from "./source_simple_diode"
 
 export const source_simple_led = source_simple_diode.extend({
   ftype: z.literal("simple_led"),
@@ -18,7 +21,7 @@ type InferredSourceSimpleLed = z.infer<typeof source_simple_led>
 /**
  * Defines a simple led component
  */
-export interface SourceSimpleLed extends SourceComponentBase {
+export interface SourceSimpleLed extends Omit<SourceSimpleDiode, "ftype"> {
   ftype: "simple_led"
   color?: string
   wavelength?: string
