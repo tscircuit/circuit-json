756317e7a6916342b9bd185d2c9ac77d1123c619
techmannih <manishchaudharyttt@gmail.com>
2025-05-26 22:47:02 +0530

    make no

--- src/source/source_simple_fuse.ts
+++ src/source/source_simple_fuse.ts
@@ -7,14 +7,20 @@ import { expectTypesMatch } from "src/utils/expect-types-match"
 
 export const source_simple_fuse = source_component_base.extend({
   ftype: z.literal("simple_fuse"),
-  current_rating: z.union([z.number(), z.string()]),
-  voltage_rating: z.union([z.number(), z.string()]).optional(),
+
+  current_rating_amps: z
+    .number()
+    .describe("Nominal current in amps the fuse is rated for"),
+
+  voltage_rating_volts: z
+    .number()
+    .describe("Voltage rating in volts, e.g. ±5V would be 5"),
 })
 
 export interface SourceSimpleFuse extends SourceComponentBase {
   ftype: "simple_fuse"
-  current_rating: number | string
-  voltage_rating?: number | string
+  current_rating_amps: number
+  voltage_rating_volts: number
 }
 
 export type SourceSimpleFuseInput = z.input<typeof source_simple_fuse>
