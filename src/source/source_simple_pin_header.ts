35aa6cf56f8a23700bf7f58cad78c361ee8e3659
techmannih <manishchaudharyttt@gmail.com>
2025-06-13 16:13:40 +0530

    add missing expectTypesMatch to simple pin header

--- src/source/source_simple_pin_header.ts
+++ src/source/source_simple_pin_header.ts
@@ -1,5 +1,6 @@
 import { source_component_base } from "src/source/base/source_component_base"
 import { z } from "zod"
+import { expectTypesMatch } from "src/utils/expect-types-match"
 
 export const source_simple_pin_header = source_component_base.extend({
   ftype: z.literal("simple_pin_header"),
@@ -7,7 +8,15 @@ export const source_simple_pin_header = source_component_base.extend({
   gender: z.enum(["male", "female"]).optional().default("male"),
 })
 
-export type SourceSimplePinHeader = z.infer<typeof source_simple_pin_header>
 export type SourceSimplePinHeaderInput = z.input<
   typeof source_simple_pin_header
 >
+type InferredSourceSimplePinHeader = z.infer<typeof source_simple_pin_header>
+
+export interface SourceSimplePinHeader extends SourceSimplePinHeaderInput {
+  ftype: "simple_pin_header"
+  pin_count: number
+  gender: "male" | "female"
+}
+
+expectTypesMatch<SourceSimplePinHeader, InferredSourceSimplePinHeader>(true)
