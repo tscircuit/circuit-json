0d8c4c2483c7e8474187ea3269b6de136ad17042
Severin Ibarluzea <seve700@gmail.com>
2025-07-27 00:15:39 -0700

    Add rotated pill hole and pad types

---
+++ tests/pcb_rotated_pill_hole_with_rect_pad.test.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "bun:test"
+import {
+  pcb_plated_hole,
+  type PcbHoleRotatedPillWithRectPad,
+} from "../src/pcb/pcb_plated_hole"
+
+test("parse rotated pill hole with rect pad", () => {
+  const hole = pcb_plated_hole.parse({
+    type: "pcb_plated_hole",
+    shape: "rotated_pill_hole_with_rect_pad",
+    hole_shape: "rotated_pill",
+    pad_shape: "rect",
+    hole_width: 1,
+    hole_height: 2,
+    hole_ccw_rotation: 45,
+    rect_pad_width: 3,
+    rect_pad_height: 4,
+    rect_ccw_rotation: 45,
+    x: 0,
+    y: 0,
+    layers: ["top"],
+  })
+  expect(hole.shape).toBe("rotated_pill_hole_with_rect_pad")
+  expect((hole as PcbHoleRotatedPillWithRectPad).hole_ccw_rotation).toBe(45)
+})
