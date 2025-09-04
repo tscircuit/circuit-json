0b42ef2b537c233af76afbd812a22d3073b73055
Severin Ibarluzea <seve700@gmail.com>
2025-09-04 09:26:46 -0700

    feat: make schematic trace source id optional

---
+++ tests/schematic_trace_optional_fields.test.ts
@@ -0,0 +1,33 @@
+import { test, expect } from "bun:test"
+import { schematic_trace } from "../src/schematic/schematic_trace"
+
+test("schematic_trace.source_trace_id is optional", () => {
+  const trace = schematic_trace.parse({
+    type: "schematic_trace",
+    schematic_trace_id: "trace1",
+    junctions: [{ x: 0, y: 0 }],
+    edges: [
+      {
+        from: { x: 0, y: 0 },
+        to: { x: 1, y: 0 },
+      },
+    ],
+  })
+  expect(trace.source_trace_id).toBeUndefined()
+})
+
+test("schematic_trace.subcircuit_connectivity_map_key is optional", () => {
+  const trace = schematic_trace.parse({
+    type: "schematic_trace",
+    schematic_trace_id: "trace2",
+    subcircuit_connectivity_map_key: "sub1",
+    junctions: [{ x: 0, y: 0 }],
+    edges: [
+      {
+        from: { x: 0, y: 0 },
+        to: { x: 1, y: 0 },
+      },
+    ],
+  })
+  expect(trace.subcircuit_connectivity_map_key).toBe("sub1")
+})
