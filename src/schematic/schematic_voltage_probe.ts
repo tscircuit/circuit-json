5377d4c0238d0e21c61de71b119de403590f0e69
Shibo <shibosoftwaredev@gmail.com>
2025-11-14 17:37:44 +0200

    feat(schematic): Add name property to schematic voltage probe (#347)

--- src/schematic/schematic_voltage_probe.ts
+++ src/schematic/schematic_voltage_probe.ts
@@ -9,6 +9,7 @@ export interface SchematicVoltageProbe {
   position: Point
   schematic_trace_id: string
   voltage?: number
+  name?: string
   subcircuit_id?: string
 }
 
@@ -19,6 +20,7 @@ export const schematic_voltage_probe = z
     position: point,
     schematic_trace_id: z.string(),
     voltage: voltage.optional(),
+    name: z.string().optional(),
     subcircuit_id: z.string().optional(),
   })
   .describe("Defines a voltage probe measurement point on a schematic trace")
