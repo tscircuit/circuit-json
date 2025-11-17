d9b0a62561b0baa3f91615f294f578e93b960fa9
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2025-11-17 21:12:16 +0200

    feat(probes): Add name property to schematic and simulation voltage probes

--- src/simulation/simulation_voltage_probe.ts
+++ src/simulation/simulation_voltage_probe.ts
@@ -9,6 +9,7 @@ export const simulation_voltage_probe = z
       "simulation_voltage_probe",
     ),
     source_component_id: z.string().optional(),
+    name: z.string().optional(),
     source_port_id: z.string().optional(),
     source_net_id: z.string().optional(),
     subcircuit_id: z.string().optional(),
@@ -37,6 +38,7 @@ export interface SimulationVoltageProbe {
   type: "simulation_voltage_probe"
   simulation_voltage_probe_id: string
   source_component_id?: string
+  name?: string
   source_port_id?: string
   source_net_id?: string
   subcircuit_id?: string
