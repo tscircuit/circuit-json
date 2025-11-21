1da2cb5d52866f23b3d5dbcdd74a44896fb68cdc
Ayush Jhawar <111112495+Ayushjhawar8@users.noreply.github.com>
2025-11-21 23:29:12 +0530

    Add source_pin_must_be_connected_error (#358)
    * Add source_pin_must_be_connected_error
    
    * Split test

---
+++ src/source/source_pin_must_be_connected_error.ts
@@ -0,0 +1,46 @@
+import { z } from "zod"
+import { getZodPrefixedIdWithDefault } from "src/common"
+import { expectTypesMatch } from "src/utils/expect-types-match"
+
+export const source_pin_must_be_connected_error = z
+  .object({
+    type: z.literal("source_pin_must_be_connected_error"),
+    source_pin_must_be_connected_error_id: getZodPrefixedIdWithDefault(
+      "source_pin_must_be_connected_error",
+    ),
+    error_type: z
+      .literal("source_pin_must_be_connected_error")
+      .default("source_pin_must_be_connected_error"),
+    message: z.string(),
+    source_component_id: z.string(),
+    source_port_id: z.string(),
+    subcircuit_id: z.string().optional(),
+  })
+  .describe(
+    "Error emitted when a pin with mustBeConnected attribute is not connected to any trace",
+  )
+
+export type SourcePinMustBeConnectedErrorInput = z.input<
+  typeof source_pin_must_be_connected_error
+>
+type InferredSourcePinMustBeConnectedError = z.infer<
+  typeof source_pin_must_be_connected_error
+>
+
+/**
+ * Error emitted when a pin with mustBeConnected attribute is not connected to any trace
+ */
+export interface SourcePinMustBeConnectedError {
+  type: "source_pin_must_be_connected_error"
+  source_pin_must_be_connected_error_id: string
+  error_type: "source_pin_must_be_connected_error"
+  message: string
+  source_component_id: string
+  source_port_id: string
+  subcircuit_id?: string
+}
+
+expectTypesMatch<
+  SourcePinMustBeConnectedError,
+  InferredSourcePinMustBeConnectedError
+>(true)
