73a4e0b6df096f4c3bdf40d3425e1a3c73683a1e
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-12-21 22:33:23 +0000

    patch

--- src/common/PcbRenderLayer.ts
+++ src/common/PcbRenderLayer.ts
@@ -2,26 +2,24 @@ import { z } from "zod"
 import { expectTypesMatch } from "src/utils/expect-types-match"
 
 export type PcbRenderLayer =
-  | "top_left"
-  | "top_center"
-  | "top_right"
-  | "center_left"
-  | "center"
-  | "center_right"
-  | "bottom_left"
-  | "bottom_center"
-  | "bottom_right"
+  | "top_silkscreen"
+  | "bottom_silkscreen"
+  | "top_copper"
+  | "bottom_copper"
+  | "top_soldermask"
+  | "bottom_soldermask"
+  | "top_fabrication"
+  | "bottom_fabrication"
 
 export const pcbRenderLayer = z.enum([
-  "top_left",
-  "top_center",
-  "top_right",
-  "center_left",
-  "center",
-  "center_right",
-  "bottom_left",
-  "bottom_center",
-  "bottom_right",
+  "top_silkscreen",
+  "bottom_silkscreen",
+  "top_copper",
+  "bottom_copper",
+  "top_soldermask",
+  "bottom_soldermask",
+  "top_fabrication",
+  "bottom_fabrication",
 ])
 
 type InferredPcbRenderLayer = z.infer<typeof pcbRenderLayer>
