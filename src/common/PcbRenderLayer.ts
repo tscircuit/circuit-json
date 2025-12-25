aade402e0433953d2b23e081a76b194846f61827
Abse2001 <abdaslam13.2.2001@gmail.com>
2025-12-25 12:10:12 +0200

    patch

--- src/common/PcbRenderLayer.ts
+++ src/common/PcbRenderLayer.ts
@@ -8,8 +8,14 @@ export type PcbRenderLayer =
   | "bottom_copper"
   | "top_soldermask"
   | "bottom_soldermask"
-  | "top_fabrication"
-  | "bottom_fabrication"
+  | "top_fabrication_note"
+  | "bottom_fabrication_note"
+  | "inner1_copper"
+  | "inner2_copper"
+  | "inner3_copper"
+  | "inner4_copper"
+  | "inner5_copper"
+  | "inner6_copper"
 
 export const pcbRenderLayer = z.enum([
   "top_silkscreen",
@@ -18,8 +24,14 @@ export const pcbRenderLayer = z.enum([
   "bottom_copper",
   "top_soldermask",
   "bottom_soldermask",
-  "top_fabrication",
-  "bottom_fabrication",
+  "top_fabrication_note",
+  "bottom_fabrication_note",
+  "inner1_copper",
+  "inner2_copper",
+  "inner3_copper",
+  "inner4_copper",
+  "inner5_copper",
+  "inner6_copper",
 ])
 
 type InferredPcbRenderLayer = z.infer<typeof pcbRenderLayer>
