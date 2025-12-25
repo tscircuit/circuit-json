71b03ef196cede5bf30ec5cd7d013335a4732f6b
Abdoaslam Allawlabi <abdaslam13.2.2001@gmail.com>
2025-12-25 12:12:24 +0200

    Added PcbRenderLayer to circuit-json common (#387)
    * Added PcbRenderLayer to circuitjson common
    
    * patch
    
    * patch

---
+++ src/common/PcbRenderLayer.ts
@@ -0,0 +1,39 @@
+import { z } from "zod"
+import { expectTypesMatch } from "src/utils/expect-types-match"
+
+export type PcbRenderLayer =
+  | "top_silkscreen"
+  | "bottom_silkscreen"
+  | "top_copper"
+  | "bottom_copper"
+  | "top_soldermask"
+  | "bottom_soldermask"
+  | "top_fabrication_note"
+  | "bottom_fabrication_note"
+  | "inner1_copper"
+  | "inner2_copper"
+  | "inner3_copper"
+  | "inner4_copper"
+  | "inner5_copper"
+  | "inner6_copper"
+
+export const pcbRenderLayer = z.enum([
+  "top_silkscreen",
+  "bottom_silkscreen",
+  "top_copper",
+  "bottom_copper",
+  "top_soldermask",
+  "bottom_soldermask",
+  "top_fabrication_note",
+  "bottom_fabrication_note",
+  "inner1_copper",
+  "inner2_copper",
+  "inner3_copper",
+  "inner4_copper",
+  "inner5_copper",
+  "inner6_copper",
+])
+
+type InferredPcbRenderLayer = z.infer<typeof pcbRenderLayer>
+
+expectTypesMatch<PcbRenderLayer, InferredPcbRenderLayer>(true)
