c954e7c62580787bed34673e1bacbb5e38ebbd2d
Abse2001 <abdaslam13.2.2001@gmail.com>
2026-01-02 16:21:52 +0200

    Added top and bottom user note layers

--- src/common/PcbRenderLayer.ts
+++ src/common/PcbRenderLayer.ts
@@ -10,6 +10,8 @@ export type PcbRenderLayer =
   | "bottom_soldermask"
   | "top_fabrication_note"
   | "bottom_fabrication_note"
+  | "top_user_note"
+  | "bottom_user_note"
   | "inner1_copper"
   | "inner2_copper"
   | "inner3_copper"
@@ -26,6 +28,8 @@ export const pcbRenderLayer = z.enum([
   "bottom_soldermask",
   "top_fabrication_note",
   "bottom_fabrication_note",
+  "top_user_note",
+  "bottom_user_note",
   "inner1_copper",
   "inner2_copper",
   "inner3_copper",
