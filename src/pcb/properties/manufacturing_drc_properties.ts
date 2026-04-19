d6fe2b4dacf03509220f23aa17aa2c506d4566dc
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-04-19 14:44:43 -0700

    Add min_track_width to manufacturing DRC properties

--- src/pcb/properties/manufacturing_drc_properties.ts
+++ src/pcb/properties/manufacturing_drc_properties.ts
@@ -2,6 +2,7 @@ import { z } from "zod"
 import { length, type Length } from "src/units"
 
 export const manufacturing_drc_properties = z.object({
+  min_track_width: length.optional(),
   min_via_to_via_spacing: length.optional(),
   min_trace_to_pad_spacing: length.optional(),
   min_pad_to_pad_spacing: length.optional(),
@@ -10,6 +11,7 @@ export const manufacturing_drc_properties = z.object({
 })
 
 export interface ManufacturingDrcProperties {
+  min_track_width?: Length
   min_via_to_via_spacing?: Length
   min_trace_to_pad_spacing?: Length
   min_pad_to_pad_spacing?: Length
