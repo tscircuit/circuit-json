c15ae9834e068c5870d5fb1fe0ac1e120f51ab3d
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-04-21 10:50:37 -0700

    Add `min_board_edge_spacing` to manufacturing DRC properties (#556)
    * Add min_board_edge_spacing manufacturing DRC property
    
    * Rename manufacturing DRC spacing properties to clearance

--- src/pcb/properties/manufacturing_drc_properties.ts
+++ src/pcb/properties/manufacturing_drc_properties.ts
@@ -3,18 +3,20 @@ import { length, type Length } from "src/units"
 
 export const manufacturing_drc_properties = z.object({
   min_trace_width: length.optional(),
-  min_via_to_via_spacing: length.optional(),
-  min_trace_to_pad_spacing: length.optional(),
-  min_pad_to_pad_spacing: length.optional(),
+  min_board_edge_clearance: length.optional(),
+  min_via_to_via_clearance: length.optional(),
+  min_trace_to_pad_clearance: length.optional(),
+  min_pad_to_pad_clearance: length.optional(),
   min_via_hole_diameter: length.optional(),
   min_via_pad_diameter: length.optional(),
 })
 
 export interface ManufacturingDrcProperties {
   min_trace_width?: Length
-  min_via_to_via_spacing?: Length
-  min_trace_to_pad_spacing?: Length
-  min_pad_to_pad_spacing?: Length
+  min_board_edge_clearance?: Length
+  min_via_to_via_clearance?: Length
+  min_trace_to_pad_clearance?: Length
+  min_pad_to_pad_clearance?: Length
   min_via_hole_diameter?: Length
   min_via_pad_diameter?: Length
 }
