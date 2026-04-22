3ccb3cadc2731dad10227ebe77c89a29174687a7
imrishabh18 <belikerishabh@gmail.com>
2026-04-22 14:56:13 -0700

    fix test

--- tests/pcb_board.test.ts
+++ tests/pcb_board.test.ts
@@ -119,18 +119,20 @@ test("pcb_board with manufacturing drc properties", () => {
     center: { x: 0, y: 0 },
     min_trace_width: "0.12mm",
     min_board_edge_clearance: "0.3mm",
-    min_via_to_via_clearance: "0.2mm",
-    min_trace_to_pad_clearance: "0.15mm",
-    min_pad_to_pad_clearance: "0.18mm",
+    min_via_hole_edge_to_via_hole_edge_clearance: "0.2mm",
+    min_plated_hole_edge_to_plated_hole_edge_clearance: "0.15mm",
+    min_trace_to_pad_edge_clearance: "0.18mm",
+    min_pad_edge_to_pad_edge_clearance: "0.18mm",
     min_via_hole_diameter: "0.25mm",
     min_via_pad_diameter: "0.45mm",
   })
 
   expect(board.min_trace_width).toBe(0.12)
   expect(board.min_board_edge_clearance).toBe(0.3)
-  expect(board.min_via_to_via_clearance).toBe(0.2)
-  expect(board.min_trace_to_pad_clearance).toBe(0.15)
-  expect(board.min_pad_to_pad_clearance).toBe(0.18)
+  expect(board.min_via_hole_edge_to_via_hole_edge_clearance).toBe(0.2)
+  expect(board.min_plated_hole_edge_to_plated_hole_edge_clearance).toBe(0.15)
+  expect(board.min_trace_to_pad_edge_clearance).toBe(0.18)
+  expect(board.min_pad_edge_to_pad_edge_clearance).toBe(0.18)
   expect(board.min_via_hole_diameter).toBe(0.25)
   expect(board.min_via_pad_diameter).toBe(0.45)
 })
