08abb77c8df183dccdafcbb652cb020edbfdf820
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-04-22 19:08:26 -0700

    Update naming of the drc properties (#562)
    * Update the drc properties
    
    * fix test
    
    * wip
    
    * wip
    
    * name updt

--- src/pcb/properties/manufacturing_drc_properties.ts
+++ src/pcb/properties/manufacturing_drc_properties.ts
@@ -4,9 +4,10 @@ import { length, type Length } from "src/units"
 export const manufacturing_drc_properties = z.object({
   min_trace_width: length.optional(),
   min_board_edge_clearance: length.optional(),
-  min_via_to_via_clearance: length.optional(),
-  min_trace_to_pad_clearance: length.optional(),
-  min_pad_to_pad_clearance: length.optional(),
+  min_via_hole_edge_to_via_hole_edge_clearance: length.optional(),
+  min_plated_hole_drill_edge_to_drill_edge_clearance: length.optional(),
+  min_trace_to_pad_edge_clearance: length.optional(),
+  min_pad_edge_to_pad_edge_clearance: length.optional(),
   min_via_hole_diameter: length.optional(),
   min_via_pad_diameter: length.optional(),
 })
@@ -14,9 +15,10 @@ export const manufacturing_drc_properties = z.object({
 export interface ManufacturingDrcProperties {
   min_trace_width?: Length
   min_board_edge_clearance?: Length
-  min_via_to_via_clearance?: Length
-  min_trace_to_pad_clearance?: Length
-  min_pad_to_pad_clearance?: Length
+  min_via_hole_edge_to_via_hole_edge_clearance?: Length
+  min_plated_hole_drill_edge_to_drill_edge_clearance?: Length
+  min_trace_to_pad_edge_clearance?: Length
+  min_pad_edge_to_pad_edge_clearance?: Length
   min_via_hole_diameter?: Length
   min_via_pad_diameter?: Length
 }
