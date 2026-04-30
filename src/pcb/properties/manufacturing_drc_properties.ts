f0e2c4dd7021b2d2de48796a5869dd3cfcc4dc86
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2026-04-30 14:07:27 -0700

    Apply suggestion from @imrishabh18

--- src/pcb/properties/manufacturing_drc_properties.ts
+++ src/pcb/properties/manufacturing_drc_properties.ts
@@ -21,8 +21,8 @@ export interface ManufacturingDrcProperties {
   min_plated_hole_drill_edge_to_drill_edge_clearance?: Length
   min_trace_to_pad_edge_clearance?: Length
   min_pad_edge_to_pad_edge_clearance?: Length
-  min_same_net_track_edge_to_track_edge_clearance?: Length
-  min_different_net_track_edge_to_track_edge_clearance?: Length
+  min_same_net_trace_edge_to_trace_edge_clearance?: Length
+  min_different_net_trace_edge_to_trace_edge_clearance?: Length
   min_via_hole_diameter?: Length
   min_via_pad_diameter?: Length
 }
