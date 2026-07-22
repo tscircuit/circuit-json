ab9883575e535749bfe476b9e4a6a8c5e0ec9abb
Severin Ibarluzea <seveibar@gmail.com>
2026-07-21 22:26:08 -0700

    add schematic component overlap warning (#655)

--- src/any_circuit_element.ts
+++ src/any_circuit_element.ts
@@ -142,6 +142,7 @@ export const any_circuit_element = z.union([
   sch.schematic_debug_object,
   sch.schematic_voltage_probe,
   sch.schematic_manual_edit_conflict_warning,
+  sch.schematic_component_overlap_warning,
   sch.schematic_group,
   sch.schematic_sheet,
   sch.schematic_table,
