0e2e10517a0a9a77d9374eabeff91b20e68a1594
seveibar <seveibar@gmail.com>
2026-08-01 14:37:20 -0700

    add source trace ids to pcb vias

--- tests/pcb_via.test.ts
+++ tests/pcb_via.test.ts
@@ -2,26 +2,30 @@ import { expect, test } from "bun:test"
 import { pcb_via, type PcbVia } from "../src/pcb/pcb_via"
 import { any_circuit_element } from "../src/any_circuit_element"
 
-test("pcb_via allows subcircuit_connectivity_map_key", () => {
+test("pcb_via allows source and subcircuit connectivity references", () => {
   const via = pcb_via.parse({
     type: "pcb_via",
     x: 1,
     y: 2,
     layers: ["top", "bottom"],
     subcircuit_connectivity_map_key: "foo",
+    source_trace_id: "source_trace_1",
   })
 
   expect(via.subcircuit_connectivity_map_key).toBe("foo")
+  expect(via.source_trace_id).toBe("source_trace_1")
 })
 
-test("any_circuit_element includes pcb_via with subcircuit_connectivity_map_key", () => {
+test("any_circuit_element includes pcb_via connectivity references", () => {
   const via = any_circuit_element.parse({
     type: "pcb_via",
     x: 1,
     y: 2,
     layers: ["top", "bottom"],
     subcircuit_connectivity_map_key: "bar",
+    source_trace_id: "source_trace_2",
   }) as PcbVia
 
   expect(via.subcircuit_connectivity_map_key).toBe("bar")
+  expect(via.source_trace_id).toBe("source_trace_2")
 })
