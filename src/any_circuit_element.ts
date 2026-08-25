1d7d5e14c04070e8b168f5e721850d594c955a7e
ShiboSoftwareDev <shibosoftwaredev@gmail.com>
2026-08-25 17:40:17 +0200

    Add schematic sheet presentation config

--- src/any_circuit_element.ts
+++ src/any_circuit_element.ts
@@ -12,7 +12,6 @@ import {
 export const any_circuit_element = z.union([
   // TODO source_config
   // TODO pcb_config
-  // TODO schematic_config
   // TODO schematic_group
   src.source_trace,
   src.source_port,
@@ -151,6 +150,7 @@ export const any_circuit_element = z.union([
   sch.schematic_element_outside_sheet_warning,
   sch.schematic_group,
   sch.schematic_sheet,
+  sch.schematic_config,
   sch.schematic_table,
   sch.schematic_table_cell,
   cad.cad_component,
